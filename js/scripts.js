46ff6ac313ef6083b9b699398c1d5cbfb8c984c4
Ethan Bear <ehwbear@icloud.com>
2021-10-29 16:06:33 -0700

    Make page pretty and blue.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ for (let index = 0; index <= userInput; index ++) {
   if (i.toString().includes("3")) {
   userArray.push("Won't you be my neighbor?");
 }
- else if (i.toString().includes("2")) {
+  else if (i.toString().includes("2")) {
   userArray.push("Boop!");
- }
- else (i.toString().includes("1")) {
+  }
+  else (i.toString().includes("1")) {
   userArray.push("Beep!");
   event.preventDefault();
- }}
+  }}
 }
 // USER-INTERFACE LOGIC
 
