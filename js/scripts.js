5b02e2de32cb58c46065e1392ecece6bb8b9165f
Ethan Bear <ehwbear@icloud.com>
2021-10-29 09:12:02 -0700

    Copy js from lng-suggester as template

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-document.ready();
-1
+$(document).ready(function() {
+
+  if (result <=7) {
+    $("#yes").fadeToggle();
+
+    // 3
+
+  } else if (result ===10) {
+    $("#no").fadeToggle();
+
+    // 2
+
+  } else {
+    $("#perhaps").fadeToggle();
+
+    // 1
+    
+  }
+  event.preventDefault();
+}
